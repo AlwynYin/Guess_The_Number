2baa7deb416f04e62335f6e89544b90368f89023
AlwynYin <73570489+AlwynYin@users.noreply.github.com>
2020-12-04 21:18:27 +0800

    Add files via upload
    ... well, some mistakes occur in the previous one.

--- Guess_number_English_version.py
+++ Guess_number_English_version.py
@@ -1,11 +1,10 @@
 import random
 print('-' * 8 + 'Guess The Number' + '-' * 8)
 # To create a loop. This variable is used for decision.
-Play_again = True
-
+Play_again = a = True
 while Play_again:
-    a = True
     # Choose the range.
+    # This if can only go once, so a burr variable is used.
     if a:
         Start = int(input("What's the smallest number?"))
         End = int(input("And the largest number?"))
@@ -13,29 +12,31 @@ while Play_again:
         # Generate integer within this range.
         Num = random.randint(Start, End)
         # This loop is used to allow the user to type in answer multiple times.
-        a -= 1
+        a = False
     Answer = input('Guess a number, from %d to %d' % (Start, End))
     # If to prevent from not typing in a number
     if Answer.isdigit():
         Answer = int(Answer)
         if Answer < Num and Start <= Answer <= End:
-            print('Nope, smaller than that.')
+            print('Nope, larger than %d.' % Answer)
         elif Answer == Num and Start <= Answer <= End:
             print('Correct.')
             while True:
                 Choice = input('Want another Game?(yes or no)')
                 # Choosing to play again or not.
                 if str.lower(Choice) == 'yes':
+                    # set a back to true so that the if will run again
+                    a = True
                     break
                 elif str.lower(Choice) == 'no':
                     print('Good bye!')
                     Play_again = False
                     break
                 else:
-                    print("Yes or no?")
+                    print("yes or no?")
                     continue
         elif Answer > Num and Start <= Answer <= End:
-            print('Nope, smaller than that.')
+            print('Nope, smaller than %d.' % Answer)
         else:
             print('Nope, it\'s from %d to %d' % (Start, End))
     else:
